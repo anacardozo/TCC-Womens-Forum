6e394cb327b50f190c43005f08f92e5ea1f65322
anacardozo <105667369+anacardozo@users.noreply.github.com>
2024-05-21 14:58:04 -0300

    finalizado

--- script.js
+++ script.js
@@ -6,22 +6,3 @@ menu.addEventListener('click', () => {
     NavMenu.classList.toggle('ativo');
 })
 
-const tabs = document.querySelectorAll('.tab-btn');
-
-tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
-
-const tabClicked = (tab) => {
-    tabs.forEach(tab => tab.classList.remove('active'));
-    tab.classList.add('active');
-
-    const contents = document.querySelectorAll('.content');
-    contents.forEach(content => content.classList.remove('show'));
-
-    const contentId = tab.getAttribute('content-id');
-    const content = document.getElementById(contentId);
-
-    content.classList.add('show');
-}
-
-const currentActiveTab = document.querySelector('.tab-btn.active');
-tabClicked(currentActiveTab);
